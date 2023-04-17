ba888fcd07d692fd54d69d4e3526dab6104af150
jibaru <ignacioruedaboada@hotmail.com>
2023-04-17 00:45:14 -0500

    feat: add get vouchers endpoint

--- routes/api/vouchers/auth.php
+++ routes/api/vouchers/auth.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\Vouchers\GetVouchersHandler;
 use App\Http\Controllers\Vouchers\Invoices\StoreInvoiceHandler;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('vouchers')->group(
     function () {
+        Route::get('/', GetVouchersHandler::class);
         Route::prefix('invoices')->group(
             function () {
                 Route::post('/', StoreInvoiceHandler::class);
